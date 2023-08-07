bb748c9373550ce87cfd71092e479a1467ad0100
technickfreak2000 <nick@technickfreak.com>
2023-08-07 02:29:07 +0200

    ap and led strip working

--- main/led_strip_ctl.c
+++ main/led_strip_ctl.c
@@ -5,23 +5,13 @@
 #include "driver/rmt_tx.h"
 #include "led_strip_encoder.h"
 
-
-#define EXAMPLE_CHASE_SPEED_MS      10
+#define EXAMPLE_CHASE_SPEED_MS 10
 
 static const char *TAG = "rmt led strip";
 
 static uint8_t led_strip_pixels[CONFIG_LED_NUMBERS * 3];
 
 TaskHandle_t ledTaskHandle = NULL;
-
-uint32_t red;
-uint32_t green;
-uint32_t blue;
-uint16_t hue;
-uint16_t start_rgb;
-rmt_channel_handle_t led_chan;
-rmt_encoder_handle_t led_encoder;
-rmt_transmit_config_t tx_config;
 
 /**
  * @brief Simple helper function, converting HSV color space to RGB color space
@@ -41,7 +31,8 @@ void led_strip_hsv2rgb(uint32_t h, uint32_t s, uint32_t v, uint32_t *r, uint32_t
     // RGB adjustment amount by hue
     uint32_t rgb_adj = (rgb_max - rgb_min) * diff / 60;
 
-    switch (i) {
+    switch (i)
+    {
     case 0:
         *r = rgb_max;
         *g = rgb_min + rgb_adj;
@@ -77,39 +68,14 @@ void led_strip_hsv2rgb(uint32_t h, uint32_t s, uint32_t v, uint32_t *r, uint32_t
 
 void task_led_strip(void *arg)
 {
-    while (1) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = i; j < CONFIG_LED_NUMBERS; j += 3) {
-                // Build RGB pixels
-                hue = j * 360 / CONFIG_LED_NUMBERS + start_rgb;
-                led_strip_hsv2rgb(hue, 100, 100, &red, &green, &blue);
-                led_strip_pixels[j * 3 + 0] = green;
-                led_strip_pixels[j * 3 + 1] = blue;
-                led_strip_pixels[j * 3 + 2] = red;
-            }
-            // Flush RGB values to LEDs
-            ESP_ERROR_CHECK(rmt_transmit(led_chan, led_encoder, led_strip_pixels, sizeof(led_strip_pixels), &tx_config));
-            ESP_ERROR_CHECK(rmt_tx_wait_all_done(led_chan, portMAX_DELAY));
-            vTaskDelay(pdMS_TO_TICKS(EXAMPLE_CHASE_SPEED_MS));
-            memset(led_strip_pixels, 0, sizeof(led_strip_pixels));
-            ESP_ERROR_CHECK(rmt_transmit(led_chan, led_encoder, led_strip_pixels, sizeof(led_strip_pixels), &tx_config));
-            ESP_ERROR_CHECK(rmt_tx_wait_all_done(led_chan, portMAX_DELAY));
-            vTaskDelay(pdMS_TO_TICKS(EXAMPLE_CHASE_SPEED_MS));
-        }
-        start_rgb += 60;
-    }
-}
-
-void init_led_strip(void)
-{
-    red = 0;
-    green = 0;
-    blue = 0;
-    hue = 0;
-    start_rgb = 0;
+    uint32_t red = 0;
+    uint32_t green = 0;
+    uint32_t blue = 0;
+    uint16_t hue = 0;
+    uint16_t start_rgb = 0;
 
     ESP_LOGI(TAG, "Create RMT TX channel");
-    led_chan = NULL;
+    rmt_channel_handle_t led_chan = NULL;
     rmt_tx_channel_config_t tx_chan_config = {
         .clk_src = RMT_CLK_SRC_DEFAULT, // select source clock
         .gpio_num = CONFIG_ESP_LED_PIN,
@@ -120,7 +86,7 @@ void init_led_strip(void)
     ESP_ERROR_CHECK(rmt_new_tx_channel(&tx_chan_config, &led_chan));
 
     ESP_LOGI(TAG, "Install led strip encoder");
-    led_encoder = NULL;
+    rmt_encoder_handle_t led_encoder = NULL;
     led_strip_encoder_config_t encoder_config = {
         .resolution = CONFIG_RMT_LED_STRIP_RESOLUTION_HZ,
     };
@@ -130,7 +96,35 @@ void init_led_strip(void)
     ESP_ERROR_CHECK(rmt_enable(led_chan));
 
     ESP_LOGI(TAG, "Start LED rainbow chase");
-    tx_config.loop_count = 0; // no transfer loop
+    rmt_transmit_config_t tx_config = {
+        .loop_count = 0, // no transfer loop
+    };
 
-    xTaskCreatePinnedToCore(task_led_strip, "task_led_strip", 4096, NULL,10, &ledTaskHandle, 1);
+    while (1)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = i; j < CONFIG_LED_NUMBERS; j += 3)
+            {
+                // Build RGB pixels
+                hue = j * 360 / CONFIG_LED_NUMBERS + start_rgb;
+                led_strip_hsv2rgb(hue, 100, 100, &red, &green, &blue);
+                led_strip_pixels[j * 3 + 0] = green;
+                led_strip_pixels[j * 3 + 1] = blue;
+                led_strip_pixels[j * 3 + 2] = red;
+            }
+            // Flush RGB values to LEDs
+            ESP_ERROR_CHECK(rmt_transmit(led_chan, led_encoder, led_strip_pixels, sizeof(led_strip_pixels), &tx_config));
+            vTaskDelay(pdMS_TO_TICKS(EXAMPLE_CHASE_SPEED_MS));
+            memset(led_strip_pixels, 0, sizeof(led_strip_pixels));
+            ESP_ERROR_CHECK(rmt_transmit(led_chan, led_encoder, led_strip_pixels, sizeof(led_strip_pixels), &tx_config));
+            vTaskDelay(pdMS_TO_TICKS(EXAMPLE_CHASE_SPEED_MS));
+        }
+        start_rgb += 60;
+    }
+}
+
+void init_led_strip(void)
+{
+    xTaskCreatePinnedToCore(task_led_strip, "task_led_strip", 4096, NULL, 10, &ledTaskHandle, 1);
 }
--- main/main.c
+++ main/main.c
@@ -25,18 +25,20 @@ static const char *TAG = "main";
 
 void app_main(void)
 {
-    //Initialize NVS
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
+  // Initialize NVS
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+  {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
 
-    ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
-    wifi_init_softap();
-    
-    ESP_LOGI(TAG, "init LED strip");
-    init_led_strip();
-    
+  ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
+  wifi_init_softap();
+
+  
+
+  ESP_LOGI(TAG, "init LED strip");
+  init_led_strip();
 }
